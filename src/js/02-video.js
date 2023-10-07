0967c6a2d876c810dabec7afc4a6c00a77d35c1b
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-10-07 12:14:11 +0300

    2homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playerElement = document.getElementById('vimeo-player');
+const VCT = 'videoplayer-current-time';
+
+const player = new Vimeo(playerElement, {
+  id: 236203659,
+  width: 640,
+});
+
+const onPlay = function (evt) {
+  localStorage.setItem(VCT, JSON.stringify(evt.seconds));
+  console.log(evt.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// Встановлюємо збережений час відтворення, або 0
+const savedTime = JSON.parse(localStorage.getItem(VCT)) || 0;
+
+player.setCurrentTime(savedTime);
